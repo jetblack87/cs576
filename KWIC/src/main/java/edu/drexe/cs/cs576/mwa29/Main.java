ff499fedcce9bc5c257fb45aa3778c6be9e0c5f0
mark <mwa29@drexel.edu>
2015-04-27 21:22:24 -0400

    Initial commit of the KWIC project.

---
+++ KWIC/src/main/java/edu/drexe/cs/cs576/mwa29/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.drexe.cs.cs576.mwa29;
+
+/**
+ * @author mark
+ *
+ */
+public class Main {
+
+	/**
+	 * The main entry into this application
+	 * @param args the command-line arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+
+}
