2cdfb79cb8e2b1eda08f4159fc2233d8829ecc58
jetblack87 <4212527+jetblack87@users.noreply.github.com>
2015-04-27 21:22:24 -0400

    Initial commit of the KWIC project.

---
+++ KWIC/src/test/java/edu/drexe/cs/cs576/mwa29/MainTest.java
@@ -0,0 +1,19 @@
+/**
+ * JUnit tests used to test Main class
+ */
+package edu.drexe.cs.cs576.mwa29;
+
+import org.junit.Test;
+
+/**
+ * @author mark
+ *
+ */
+public class MainTest {
+
+	@Test
+	public void test() {
+		Main.main(new String[]{});
+	}
+
+}
